7b4a46d7f5ccb4a628af7148f00d10136ad7be33
toshiki.tokio <toshiki.tokio@rakus-partners.co.jp>
2025-11-21 13:36:30 +0900

    Add output for security group ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,3 +99,8 @@ output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web_server.public_ip
 }
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
+  description = "Security Group ID for SSH access"
+}
